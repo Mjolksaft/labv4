daca3401f893e59fa127bddbc9204a412b0b38e5
Mjolksaft <Kalla01@hotmail.se>
2023-03-20 19:46:31 +0100

    added env token and views

--- database.js
+++ database.js
@@ -3,10 +3,21 @@ const db = new sqlite3.Database("db")
 
 const init = () => {
     db.serialize(() => {
-        db.run("CREATE TABLE IF NOT EXISTS user (username, password)")
+        db.run("CREATE TABLE IF NOT EXISTS Users(userID INTEGER NOT NULL, role, name, password, PRIMARY KEY(userID))") // primary key already unique  
+        // db.run("INSERT INTO Users(role,name,password) VALUES('admin', 'admin', 'admin'), ('user1', 'student', 'password'), ('user2', 'student', 'password2'), ('teacher', 'teacher', 'password3')")
+
+    })
+}
+const getUsers = () => {
+    return new Promise((res,rej) => {
+        db.all("SELECT * FROM Users", (err, rows) => {
+            if(err) rej(err)
+            res(rows)
+        })
     })
 }
 
 module.exports = {
     init,
+    getUsers
 }
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require("express")
 const app = express()
 const db = require("./database")
+const jwt = require("jsonwebtoken")
+require("dotenv").config()
 
 app.set("view-engine", "ejs")
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
-app.get("/", (req,res) => {
-    res.render("view.ejs")
+app.get("/users", async (req,res) => {
+    await db.getUsers()
+    .then((result) => {
+        res.json(result)
+    })
+})
+
+app.get("/admin", (req,res) => {
+    res.render("admin.ejs")
 })
 
 app.listen(5000, () => {
